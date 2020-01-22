aa391f6928515dd3f215e89bef91c1faa444aa63
itsmetechy@gmail.com <sivashankar.kuppusamy@keypathedu.com>
2020-01-22 23:07:31 +0000

    Nav

---
+++ src/Components/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Root = (props) => {
+        return (
+            <div>
+                <div> {'Hi'} </div>
+                {/* <div>{ props.children  }</div> */}
+            </div>
+        )
+    }
+
+export default Root
